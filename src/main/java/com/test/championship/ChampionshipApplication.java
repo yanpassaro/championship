97aa50384fbc80ef0da02b81da1951fb16aed069
Yan <yannpassaro@gmail.com>
2023-03-20 19:19:12 -0300

    Initial commit

---
+++ src/main/java/com/test/championship/ChampionshipApplication.java
@@ -0,0 +1,13 @@
+package com.test.championship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/championship/ChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.championship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
